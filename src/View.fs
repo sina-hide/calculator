46079d80e1945cb1ae77fb6171c0e84f65a610fb
Sina Hinderks <sina@hinderks.net>
2019-03-30 19:45:04 +0100

    View deutlich verbessert
    Die Buttons sind jetzt wie ursprünglich vorgesehen jeweils in einer
    eigenen Zelle statt alle Buttons einer Zeile in einer Zelle.
    
    Durch den Einsatz von CSS konnte der Taschenrechner optisch aufgewertet
    werden.  Insgesamt ist er größer und die Beschriftungen der Tasten sind
    ordentlich zentriert.
    
    Der Code ist kürzer, weil jetzt viele kleinere Funktionen benutzt
    werden, statt in einem Rutsch den ganzen View mit vielen Wiederholungen
    zu generieren.

--- src/View.fs
+++ src/View.fs
@@ -4,93 +4,57 @@ open Calculator.Model
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
+let keyLayout =
+    [
+        ["7", Digit 7;  "8", Digit 8;  "9", Digit 9;            "C", ClearKey]
+        ["4", Digit 4;  "5", Digit 5;  "6", Digit 6;            "/", Operator Division]
+        ["1", Digit 1;  "2", Digit 2;  "3", Digit 3;            "*", Operator Multiplication]
+        ["0", Digit 0;  "=", EqualKey; "+", Operator Addition;  "-", Operator Subtraction]
+    ]
+
+let viewButton (caption, key) dispatch =
+    button
+        [
+            OnClick (fun _ -> dispatch (KeyPress key))
+            Style [
+                Margin "0 auto"
+                Padding "0"
+            ]
+        ]
+        [
+            div
+                [
+                    Style [
+                        Height 40
+                        Width 40
+                        Display "table-cell"
+                        TextAlign "center"
+                        VerticalAlign "middle"
+                        FontSize 20
+                        Margin "0 auto"
+                        Padding "0"
+                    ]
+                ]
+                [str caption]
+        ]
+
+let viewCell keyCell dispatch =
+    td [] [viewButton keyCell dispatch]
+
+let viewRow keyRow dispatch =
+    let elements = keyRow |> List.map (fun keyCell -> viewCell keyCell dispatch)
+    tr [] elements
+
+let viewTable keyTable dispatch =
+    let elements = keyTable |> List.map (fun keyRow -> viewRow keyRow dispatch)
+    table [] [tbody [] elements]
+
 let view model dispatch =
     div
         []
         [
-            str (display model)
-            table
-                []
-                [
-                    tr
-                        []
-                        [
-                            td
-                                []
-                                [
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 7)))]
-                                        [str "7"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 8)))]
-                                        [str "8"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 9)))]
-                                        [str "9"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress ClearKey))]
-                                        [str "C"]
-                                ]
-                        ]
-                    tr
-                        []
-                        [
-                            td
-                                []
-                                [
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 4)))]
-                                        [str "4"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 5)))]
-                                        [str "5"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 6)))]
-                                        [str "6"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Operator Division)))]
-                                        [str "/"]
-                                ]
-                        ]
-                    tr
-                        []
-                        [
-                            td
-                                []
-                                [
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 1)))]
-                                        [str "1"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 2)))]
-                                        [str "2"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 3)))]
-                                        [str "3"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Operator Multiplication)))]
-                                        [str "*"]
-                                ]
-                        ]
-                    tr
-                        []
-                        [
-                            td
-                                []
-                                [
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Digit 0)))]
-                                        [str "0"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress EqualKey))]
-                                        [str "="]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Operator Addition)))]
-                                        [str "+"]
-                                    button
-                                        [OnClick (fun _ -> dispatch (KeyPress (Operator Subtraction)))]
-                                        [str "-"]
-                                ]
-                        ]
-                ]
+            div
+                [Style [FontSize 20]]
+                [str (display model)]
+            viewTable keyLayout dispatch
         ]
